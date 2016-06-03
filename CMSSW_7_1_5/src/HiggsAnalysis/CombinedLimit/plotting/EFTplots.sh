6e2cb97bba441907504372e3937169feac7d33e4
mzgubic <miha.zgubic@gmail.com>
2016-06-03 14:02:43 +0200

    Update EFTplots.sh

--- CMSSW_7_1_5/src/HiggsAnalysis/CombinedLimit/plotting/EFTplots.sh
+++ CMSSW_7_1_5/src/HiggsAnalysis/CombinedLimit/plotting/EFTplots.sh
@@ -28,7 +28,7 @@
 
 #scarcer data
 # D5
-python plot4EFTs.py D5a/combined_D5a_3000fb.root D5a/combined_D5a_300fb.root D5a/combined_D5a_20fb.root D5b/combined_D5b_3000fb.root D5b/combined_D5b_300fb.root D5b/combined_D5b_20fb.root D5c/combined_D5c_3000fb.root D5c/combined_D5c_300fb.root D5c/combined_D5c_20fb.root D5d/combined_D5d_3000fb.root D5d/combined_D5d_300fb.root D5d/combined_D5d_20fb.root
+#python plot4EFTs.py D5a/combined_D5a_3000fb.root D5a/combined_D5a_300fb.root D5a/combined_D5a_20fb.root D5b/combined_D5b_3000fb.root D5b/combined_D5b_300fb.root D5b/combined_D5b_20fb.root D5c/combined_D5c_3000fb.root D5c/combined_D5c_300fb.root D5c/combined_D5c_20fb.root D5d/combined_D5d_3000fb.root D5d/combined_D5d_300fb.root D5d/combined_D5d_20fb.root
 #python plot2EFTs.py D5c/combined_D5c_3000fb.root D5c/combined_D5c_300fb.root D5c/combined_D5c_20fb.root D5d/combined_D5d_3000fb.root D5d/combined_D5d_300fb.root D5d/combined_D5d_20fb.root
 # D6
 #python plot2EFTs.py D6a/combined_D6a_3000fb.root D6a/combined_D6a_300fb.root D6a/combined_D6a_20fb.root D6b/combined_D6b_3000fb.root D6b/combined_D6b_300fb.root D6b/combined_D6b_20fb.root
